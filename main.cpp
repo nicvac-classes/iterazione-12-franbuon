3ab6e112470699f5a8ef9558c63bdeffe4de2c89
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-10-05 15:23:25 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float costo, risp, nuovorisp, ciframancante, cifraaggiuntiva;
+    int i;
+    cout<<"Quanto costa la moto?"<<endl;
+    cin>>costo;
+    cout<<"Quanto hai risparmiato?"<<endl;
+    cin>>risp;
+    i=0;
+    while (costo>risp)
+    {
+         cout<<"Quanto hai risparmiato oggi?"<<endl;
+        cin>>nuovorisp;
+        risp=risp+nuovorisp;
+        ciframancante=costo-risp;
+        cout="La cifra mancante è "<<ciframancante<<"€"<<endl;
+        i=i+1;
+    }
+    cifraaggiuntiva=risp-costo;
+    cout<<"Hai risparmiato per "<<i<<" giorni"<<endl;
+    cout<<"Hai risparmiato "<<risp<<"€"<<endl;
+    cout<<"Ti sono avanzati "<<cifraaggiuntiva<<"€";
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
